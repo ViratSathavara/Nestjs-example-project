b7be334ce686eb2f2c77db76c3676b78bd1facb0
viratwoyce <vsathavara@woyce.io>
2025-02-09 12:21:00 +0530

    done

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Virat';
+    return 'Hello Virat, How are you?';
   }
 }
